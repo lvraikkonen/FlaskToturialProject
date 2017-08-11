842a989f1ccba6af4877e92e5afa800b80d47324
Claus Lv <claus.lv@hotmail.com>
2017-08-11 16:35:46 +0800

    Flask集成Bootstrap框架

--- hello.py
+++ hello.py
@@ -6,13 +6,16 @@ from flask import request
 from flask import url_for
 from flask import render_template
 
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    return "<h1>Index Page</h1>Hello World!\n<p>Your browser is %s</p>" % user_agent
+    return render_template('index.html', user_agent=user_agent)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -31,11 +34,6 @@ def show_the_login_form():
     return "Showing login form..."
 
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return "Post %d" % post_id
-
-
 @app.route('/sum/<int:x>/<int:y>')
 def sum_param(x, y):
     return "%d + %d = %d" % (x, y, x+y)
@@ -44,7 +42,22 @@ def sum_param(x, y):
 @app.route('/hello/')
 @app.route('/hello/<user_name>')
 def hello(user_name = None):
-    return render_template('hello_user.html', name=user_name)
+    return render_template('user.html', name=user_name)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return render_template('post.html', post_id=post_id)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 with app.test_request_context():
@@ -53,9 +66,10 @@ with app.test_request_context():
     print url_for('login', next='/')
     print url_for('show_post', post_id=123)
     print url_for('sum_param', x=1, y=2)
+    print url_for('hello', user_name='Claus')
 
 print app.url_map
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
