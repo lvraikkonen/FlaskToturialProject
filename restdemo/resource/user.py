a505066336db00ac9b9f097deac0dd4e1ae05ed3
Shuo Lv <claus.lv@hotmail.com>
2019-10-24 10:16:06 +0800

    add post\delete\put methods

--- restdemo/resource/user.py
+++ restdemo/resource/user.py
@@ -18,7 +18,56 @@ class User(Resource):
         if user:
             return user.as_dict()
         # user not found
-        return {'message': f"user {username} not found"}
+        return {'message': f"user {username} not found"}, 404
+
+    def post(self, username):
+        """
+        create a user
+        :param username:
+        :return:
+        """
+        data = User.parser.parse_args()
+        user = db.session.query(UserModel).filter(UserModel.username==username).first()
+        if user:
+            return {'message': f"user {username} already exist."}
+        user = UserModel(
+            username=username,
+            password_hash=data.get('password')
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user.as_dict(), 201
+
+    def delete(self, username):
+        """
+        delete a user by username
+        :param username:
+        :return:
+        """
+        user = db.session.query(UserModel).filter(UserModel.username==username).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return {'message': f"user {username} deleted."}
+        else:
+            return {'message': f"user {username} not found."}, 204
+
+    def put(self, username):
+        """
+        update a user password
+        :param username:
+        :return:
+        """
+        user = db.session.query(UserModel).filter(
+            UserModel.username == username
+        ).first()
+        if user:
+            data = User.parser.parse_args()
+            user.password_hash = data.get('password')
+            db.session.commit()
+            return user.as_dict()
+        else:
+            return {'message': f"user {username} not found."}, 204
 
 
 class Users(Resource):
